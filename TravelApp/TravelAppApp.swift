ff2396cdba579081143fd58ec39d79835cb5c0cc
Alex Beattie <artrisanbranding@me.com>
2020-11-17 17:10:52 -0800

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Alex Beattie on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
